9bcfdd7d36e4b5e967f229ee889c6ab0cb2be5e4
rx_jinjunjie <sweet_dreams@aliyun.com>
2014-09-10 17:45:20 +0800

    visitor comment

--- src/test/java/pac/pattern/visitor/Visitor.java
+++ src/test/java/pac/pattern/visitor/Visitor.java
@@ -1,9 +1,9 @@
 package pac.pattern.visitor;
 
-import pac.pattern.visitor.concrete.AlvezElement;
-import pac.pattern.visitor.concrete.ConcreteElement;
-
+/**
+ * Represent an operation to be performed on the elements of an object structure.
+ * Visitor lets you define a new operation without changing the classes of the elements of which it operates.
+ */
 public interface Visitor {
-    void visit(ConcreteElement element);
-    void visit(AlvezElement element);
+    void visit(Element element);
 }
--- src/test/java/pac/pattern/visitor/concrete/ConcreteVisitor.java
+++ src/test/java/pac/pattern/visitor/concrete/ConcreteVisitor.java
@@ -1,16 +1,13 @@
 package pac.pattern.visitor.concrete;
 
+import pac.pattern.visitor.Element;
 import pac.pattern.visitor.Visitor;
 
 public class ConcreteVisitor implements Visitor{
     @Override
-    public void visit(AlvezElement element) {
-        System.out.println("concrete visitor got Alvez");
+    public void visit(Element element) {
         element.doSomething();
     }
 
-    @Override
-    public void visit(ConcreteElement element) {
-        element.doSomething();
-    }
+
 }
